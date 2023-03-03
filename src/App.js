fb9d4ec9a99d720f8ac5e9bd9e670991b3c005bf
Abdi Hersi <abdi_shakur23@hotmail.com>
2023-03-03 00:22:56 -0700

    worked on video side bar

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import Form from "./components/Form/Form";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import VideoDescription from "./components/VideoDescription/VideoDescription";
+import VideoSideBar from "./components/VideoSideBar/VideoSideBar";
 import data from "./data/video-details.json";
+import datalist from "./data/videos.json";
+import "./App.scss";
 function App() {
+  let change;
   return (
     <>
       <Header />
       <Hero />
-      <VideoDescription />
-      <Form />
-      <Comments data={data} />
+      <div className="desktop">
+        <div className="desktop__main">
+          <VideoDescription />
+          <Form />
+          <Comments data={data} />
+        </div>
+        <div className="desktop__side">
+          <VideoSideBar datalist={datalist} change={change} />
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/components/VideoSideBar/VideoSideBar.js
@@ -0,0 +1,22 @@
+import "./VideoSideBar.scss";
+const VideoSideBar = ({ datalist, change }) => {
+  return (
+    <div className="videoside">
+      <p className="videoside__title">NEXT VIDEOS</p>
+      {datalist.map((number) => {
+        return (
+          <div className="videoside__container" onclick={change}>
+            <img className="videoside__image" src={number.image} />
+
+            <div className="videoside__text">
+              <p className="videoside__header">{number.title}</p>
+              <p className="videoside__channel">{number.channel}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default VideoSideBar;
