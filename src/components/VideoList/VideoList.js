160ef8492fa7b9cecd2e41b07225c175472601fa
Abdi Hersi <abdi_shakur23@hotmail.com>
2023-03-10 14:22:36 -0700

    Fixed axios data flow

--- src/components/VideoList/VideoList.js
+++ src/components/VideoList/VideoList.js
@@ -1,143 +1,9 @@
-// import Hero from "../Hero/Hero";
-// import "../../App.scss";
-// import axios from "axios";
-// import { useState, useEffect } from "react";
-// import "../Home/Home";
-// import VideoDescription from "../VideoDescription/VideoDescription";
-// import Comments from "../Comments/Comments";
-// import Form from "../Form/Form";
-// import "./VideoList.scss";
-// const VideoList = ({ data, currentVideo }) => {
-//   //check for ids. if they are the same then we know which video details to get
-
-//   console.log({ currentVideo });
-//   // currentvideo:
-//   function getVideoDetails() {
-//     const currentVideoId = currentVideo.id;
-//     const currentVideoDetails = data.find((videoDetail) => {
-//       return videoDetail.id === currentVideoId;
-//     });
-//     return currentVideoDetails;
-//   }
-//   const { title, channel, description, timestamp, views, likes, comments } =
-//     getVideoDetails();
-//   return (
-//     //Videolist component
-//     <div className="videoList">
-//       <div className="videoList__contain">
-//         <VideoDescription
-//           title={title}
-//           channel={channel}
-//           timestamp={timestamp}
-//           views={views}
-//           likes={likes}
-//           description={description}
-//         />
-//         <Form />
-//         <Comments comments={comments} />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default VideoList;
-
-// const VideoList = ({ currentVideo }) => {
-//check for ids. if they are the same then we know which video details to get
-
-// const api_url =
-//   "https://project-2-api.herokuapp.com/videos/84e96018-4022-434e-80bf-000ce4cd12b8?api_key=9a394b9c-6c4f-4413-bd66-603a19fcc11a";
-// const [currentVideo, setCurrentVideo] = useState([]);
-// useEffect(() => {
-//   axios
-//     .get(api_url)
-
-//     .then((resp) => {
-//       setCurrentVideo(resp.data);
-//     })
-//     .catch((error) => {
-//       <h1>hi</h1>;
-//     });
-// }, []);
-
-// currentvideo:
-// function getVideoDetails() {
-//   const currentVideoId = currentVideo.id;
-//   const currentVideoDetails = data.find((videoDetail) => {
-//     return videoDetail.id === currentVideoId;
-//   });
-//   return currentVideoDetails;
-// }
-// const { title, channel, description, timestamp, views, likes, comments } =
-//   getVideoDetails();
-
-//   return (
-//     //Videolist component
-//     <div className="videoList">
-//       <div className="videoList__contain">
-//         <VideoDescription
-//           title={currentVideo.title}
-//           channel={currentVideo.channel}
-//           timestamp={currentVideo.timestamp}
-//           views={currentVideo.views}
-//           likes={currentVideo.likes}
-//           description={currentVideo.description}
-//         />
-//         <Form />
-//         <Comments comments={currentVideo.comments} />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default VideoList;
-
-// import Hero from "../Hero/Hero";
-// import "../../App.scss";
-// import VideoDescription from "../VideoDescription/VideoDescription";
-// import Comments from "../Comments/Comments";
-// import Form from "../Form/Form";
-// import "./VideoList.scss";
-// const VideoList = ({ data, currentVideo }) => {
-//   //check for ids. if they are the same then we know which video details to get
-//   // currentvideo:
-//   function getVideoDetails() {
-//     const currentVideoId = currentVideo.id;
-//     const currentVideoDetails = data.find((videoDetail) => {
-//       return videoDetail.id === currentVideoId;
-//     });
-//     return currentVideoDetails;
-//   }
-//   const { title, channel, description, timestamp, views, likes, comments } =
-//     getVideoDetails();
-//   return (
-//     //Videolist component
-//     <div className="videoList">
-//       <div className="videoList__contain">
-//         <VideoDescription
-//           title={title}
-//           channel={channel}
-//           timestamp={timestamp}
-//           views={views}
-//           likes={likes}
-//           description={description}
-//         />
-//         <Form />
-//         <Comments comments={comments} />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default VideoList;
-
 import "../../App.scss";
 import VideoDescription from "../VideoDescription/VideoDescription";
 import Comments from "../Comments/Comments";
 import Form from "../Form/Form";
 import "./VideoList.scss";
 import Hero from "../Hero/Hero";
-
 import axios from "axios";
 import { useEffect, useState } from "react";
 
@@ -187,23 +53,24 @@ const VideoList = ({ idOfVideoToDisplay }) => {
   } = videoDetail;
   return (
     //Videolist component
-    <>
-      <div className="videoList">
+
+    <div className="videoList">
+      <div className="videoList__image">
         <Hero image={image} />
-        <div className="videoList__contain">
-          <VideoDescription
-            title={title}
-            channel={channel}
-            timestamp={timestamp}
-            views={views}
-            likes={likes}
-            description={description}
-          />
-          <Form />
-          <Comments comments={comments} />
-        </div>
       </div>
-    </>
+      <div className="videoList__contain">
+        <VideoDescription
+          title={title}
+          channel={channel}
+          timestamp={timestamp}
+          views={views}
+          likes={likes}
+          description={description}
+        />
+        <Form />
+        <Comments comments={comments} />
+      </div>
+    </div>
   );
 };
 
